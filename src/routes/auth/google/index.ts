27be26fd155249946440b22ae54b015b96913a9e
Lcortbnzzy <vincentjumadiao1@gmail.com>
2025-11-06 05:23:54 +0800

    From GET to get

--- src/routes/auth/google/index.ts
+++ src/routes/auth/google/index.ts
@@ -1,8 +1,7 @@
 import type { Handler } from 'express'
 import { getAuthorizationUrl } from '@utils/oauth/google'
 
-// ✅ Change to uppercase GET
-export const GET: Handler = (req, res) => {
+export const get: Handler = (req, res) => {
     const state = (req.query.state as string) || undefined
     const url = getAuthorizationUrl(req, state)
 
