c89aa4c0283c2402c2ecef8bc78a0a05351714a9
Lcortbnzzy <vincentjumadiao1@gmail.com>
2025-11-07 08:05:57 +0800

    changes in routes/index.ts

--- src/routes/save/index.ts
+++ src/routes/save/index.ts
@@ -1,4 +1,8 @@
 import { getSave, updateSave, resetSave } from '@controllers/save'
+import middlewares from '@middlewares'
+
+// ✅ IMPORTANT: Add authentication middleware
+export const middleware = [middlewares.authenticate]
 
 export const get = getSave
 export const put = updateSave
